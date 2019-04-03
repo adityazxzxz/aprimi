9775843d68f3286b82bddde5f6a7c49899ed7b9f
adityazxzxz <adityapratamax11@gmail.com>
2019-04-03 16:25:13 +0700

    update verify dir

--- application/controllers/Folder_management.php
+++ application/controllers/Folder_management.php
@@ -5,7 +5,6 @@ class Folder_management extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		//load model
-		$this->load->model('Auth_model','auth');
 		$this->load->helper('auth_helper');
 		check_session();
 	}
@@ -19,14 +18,15 @@ class Folder_management extends CI_Controller {
 		if(!empty($folder)){
 			$path .=$folder.'/';
 		}
+		if(!is_dir($path)){
+			redirect('folder_management/list');
+		}
 		$scan = scandir($path);
 		$scanned = array_diff($scan, array('..', '.'));
 		$data['mainpage'] = 'mainpage/folder_management';
 		$data['path'] = $path;
 		$data['list_file'] = $scanned;
 		$this->load->view('index',$data);
-		/*foreach ($scanned as $dir => $value) {
-			echo $value;if(is_dir($path.$value)) echo "directory"; else echo "bukan dir";echo date("F d Y H:i:s",filemtime($path.$value))."<br>";
-		}*/
 	}
+
 }
